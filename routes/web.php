451b570e7b697079c6ad12752a0f8fb6d57674bb
Stephan Wagner <stephanwagner.me@gmail.com>
2025-11-02 12:47:40 +0900

    Refactor routes

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return "Zaplish is installed. Frontend is handled by zaplish/core.";
-});
---
+++ themes/zaplish/routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Theme Routes
+|--------------------------------------------------------------------------
+|
+| These routes are loaded automatically by the Zaplish core
+| if the current theme is active. You can register extra routes
+| or override the default FrontendController routes here.
+|
+*/
+
+// TODO TEST
+// Route::get('/contact', [Themes\Zaplish\Controllers\ContactController::class, 'index'])->name('contact');
+// Route::view('/about', 'theme::page-about')->name('about');
